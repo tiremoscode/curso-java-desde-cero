64a571bb96efde48e761993d1ecd02844f27d70b
Alberto Luebbert M. <alberto@ideashappy.com>
2026-02-17 21:03:06 -0600

    :sparkles: Clases 1 y 2

---
+++ dia01/HolaMundo.java
@@ -0,0 +1,7 @@
+package dia01;
+
+public class HolaMundo {
+    public static void main(String args[]) {
+        System.out.println("Hola Mundo - Curso TecGurus de Java!!!");
+    }
+}
---
+++ dia02/Variables.java
@@ -0,0 +1,13 @@
+package dia02;
+
+public class Variables {
+    static int edad = 30;
+    static String nombre = "Alberto";
+    static double salario = 15000.50;
+
+    public static void main(String[] args) {
+        System.out.println("Mi edad es " + edad);
+        System.out.println("Mi nombre es " + nombre);
+        System.out.println("Mi salario es " + salario);
+    }
+}
